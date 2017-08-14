a5def07a69be7d9f9b3c8049ab69f66dff7cf6f8
Jeremy Cariaga <jcariaga438@robertmorris.edu>
2017-08-13 21:38:12 -0500

    package

---
+++ server.js
@@ -0,0 +1,25 @@
+app.post(/friends, rew. res){
+ //temp best friends
+    var bestFriend = {
+        name: '',
+        photo: '',
+        difference: Infinity
+    }
+    
+    var me = req.body;
+    loop through db of friends {
+        var totaldiff = 0; 
+        var currentFriend = friends[i]
+        var J = 0 loop through scores  currentFriend.scores j++
+            currentFriendScore = currentFriends.scores[j];
+            me.scorescurrentFriendScore - me.score
+            Abs(currentFriendScore - me.score)
+            if(totaldif < bestFriend.difference) TRUE FIRST TimeRanges
+                bestFriend.name = currentFriend.name
+
+    }
+
+    
+}
+
+//   WISHES = EATDABURGER
